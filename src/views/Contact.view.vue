15ca61f43b12d1b4630985b258a82717f733c904
= <=>
2024-07-22 23:58:01 -0400

    Added Form Submission

--- src/views/Contact.view.vue
+++ src/views/Contact.view.vue
@@ -1,5 +1,30 @@
 <script setup lang="ts">
 import Header from '@/components/header/Header.component.vue';
+
+const handleSubmit = (e: Event) => {
+  const formData = new FormData(e.target as HTMLFormElement)
+  const honey = formData.get('honey')
+
+  if (honey) throw new Error('Bot detected!')
+
+  const email = formData.get('email')
+  const message = formData.get('message')
+
+  fetch('https://formsubmit.co/ajax/adrydevmateo@gmail.com', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      email,
+      message
+    })
+  })
+    .then(res => res.json())
+    .then(data => console.log('Data: ', data))
+    .catch(err => { throw new Error(err) })
+}
 </script>
 
 <template>
@@ -22,9 +47,13 @@ import Header from '@/components/header/Header.component.vue';
         </div>
       </div>
 
-      <form class="contact-form">
-        <input type="email" name="email" id="contact__email" placeholder="Email">
-        <textarea name="message" id="contact__message" placeholder="Message" rows="10" maxlength="750"></textarea>
+      <form class="contact-form" netlify @submit.prevent="handleSubmit">
+        <div class="contact-form__content">
+          <input type="text" name="honey" style="display: none;">
+          <input type="email" name="email" id="contact__email" placeholder="Email" autocomplete="off">
+          <textarea name="message" id="contact__message" placeholder="Message" rows="10" maxlength="750"></textarea>
+        </div>
+        <button class="submit__btn" type="submit">Send</button>
       </form>
     </div>
   </div>
@@ -66,8 +95,13 @@ import Header from '@/components/header/Header.component.vue';
 }
 
 .contact-form {
-  background-color: var(--color-accent-muted);
   max-width: 738px;
+  display: flex;
+  flex-direction: column;
+}
+
+.contact-form__content {
+  background-color: var(--color-accent-muted);
   display: grid;
   gap: 10px;
   margin-top: 40px;
@@ -76,7 +110,8 @@ import Header from '@/components/header/Header.component.vue';
 #contact__email,
 #contact__message,
 #contact__email::placeholder,
-#contact__message::placeholder {
+#contact__message::placeholder,
+.submit__btn {
   font-weight: var(--font-weight-extra-bold);
   color: var(--color-secondary-muted);
   transition: color var(--transition-primary);
@@ -109,12 +144,22 @@ import Header from '@/components/header/Header.component.vue';
   resize: none;
 }
 
+.submit__btn {
+  background-color: transparent;
+  place-self: end;
+  border: none;
+  margin-top: 22px;
+  padding: 10px 12px;
+  cursor: pointer;
+}
+
 @media (hover:hover) {
 
   #contact__email:hover,
   #contact__message:hover,
   #contact__email:hover::placeholder,
-  #contact__message:hover::placeholder {
+  #contact__message:hover::placeholder,
+  .submit__btn:hover {
     color: var(--color-secondary);
   }
 }
